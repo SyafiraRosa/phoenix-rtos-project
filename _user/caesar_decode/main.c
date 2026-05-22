503c4c1fb40488253606e4b10f530d7afdf513f4
SyafiraRosa <firarosa966@gmail.com>
2026-05-22 21:34:53 +0200

    feat: implement Caesar Cipher encode and decode servers

---
+++ _user/caesar_decode/main.c
@@ -0,0 +1,207 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Caesar Cipher Decoder Server
+ *
+ * This server decodes plain text using the Caesar cipher with a shift of 5.
+ * Created by Syafira Rosa
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+
+/* Phoenix-RTOS messaging headers */
+#include <sys/msg.h>
+
+/* For create_dev() */
+#include <posix/utils.h>
+
+#define BUF_SIZE 4096
+#define CIPHER_SHIFT 5
+
+/* Internal storage for the plain-text */
+static char storage_buf[BUF_SIZE];
+static size_t storage_len = 0;
+
+
+/**
+ * Apply Caesar cipher shift backward to a single character.
+ */
+static char shift_char_backward(char character)
+{
+	if (character >= 'A' && character <= 'Z') {
+		/* Upper case shifting backward with alphabetical wrap-around */
+		int decoded_index = (character - 'A' - CIPHER_SHIFT + 26) % 26;
+		return 'A' + decoded_index;
+	}
+	else if (character >= 'a' && character <= 'z') {
+		/* Lower case shifting backward with alphabetical wrap-around */
+		int decoded_index = (character - 'a' - CIPHER_SHIFT + 26) % 26;
+		return 'a' + decoded_index;
+	}
+	else {
+		/* Do not modify numbers, spaces, and punctuation symbols */
+		return character;
+	}
+}
+
+
+/**
+ * Process a whole stream of characters by applying the backward shift.
+ */
+static void process_caesar_decode(const char *source, char *destination, size_t len)
+{
+	for (size_t i = 0; i < len; i++) {
+		destination[i] = shift_char_backward(source[i]);
+	}
+}
+
+
+static int handle_device_open(oid_t *device_oid)
+{
+	(void)device_oid;
+
+	/* Reset buffers and counters when device is opened */
+	storage_len = 0;
+	memset(storage_buf, 0, BUF_SIZE);
+
+	return 0;
+}
+
+
+static int handle_device_close(oid_t *device_oid)
+{
+	(void)device_oid;
+	return 0;
+}
+
+
+static ssize_t handle_device_read(oid_t *device_oid, void *user_buffer, size_t requested_len, off_t read_offset)
+{
+	(void)device_oid;
+
+	if (read_offset < 0) {
+		return -EINVAL;
+	}
+
+	if ((size_t)read_offset >= storage_len) {
+		/* No more data to read */
+		return 0;
+	}
+
+	size_t remaining_bytes = storage_len - (size_t)read_offset;
+	size_t bytes_to_copy = (requested_len < remaining_bytes) ? requested_len : remaining_bytes;
+
+	/* Copy processed plain-text to the requesting user space */
+	memcpy(user_buffer, storage_buf + read_offset, bytes_to_copy);
+
+	return (ssize_t)bytes_to_copy;
+}
+
+
+static ssize_t handle_device_write(oid_t *device_oid, const void *user_data, size_t data_len, off_t write_offset)
+{
+	(void)device_oid;
+	(void)write_offset;
+
+	if (data_len == 0) {
+		return 0;
+	}
+
+	if (data_len > BUF_SIZE) {
+		/* Limit inputs to prevent buffer overflows */
+		return -ENOMEM;
+	}
+
+	/* Perform decryption and update buffer */
+	process_caesar_decode((const char *)user_data, storage_buf, data_len);
+	storage_len = data_len;
+
+	printf("caesar_decode server: Successfully decrypted %zu bytes\n", data_len);
+
+	return (ssize_t)data_len;
+}
+
+
+__attribute__((noreturn)) static void run_server_message_loop(oid_t *server_oid)
+{
+	msg_t message;
+	msg_rid_t request_id;
+
+	for (;;) {
+		int status = msgRecv(server_oid->port, &message, &request_id);
+		if (status < 0) {
+			if (status == -EINTR) {
+				/* Retry if interrupted by signal handler */
+				continue;
+			}
+			else {
+				/* Shutdown server on critical IPC failures */
+				fprintf(stderr, "caesar_decode: Critical error in msgRecv: %d (%s)\n",
+					status, strerror(status));
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		/* Message processing dispatcher */
+		switch (message.type) {
+			case mtOpen:
+				message.o.io.err = handle_device_open(&message.i.openclose.oid);
+				break;
+
+			case mtClose:
+				message.o.io.err = handle_device_close(&message.i.openclose.oid);
+				break;
+
+			case mtRead:
+				message.o.io.err = handle_device_read(&message.i.io.oid,
+					message.o.data, message.o.size, message.i.io.offs);
+				break;
+
+			case mtWrite:
+				message.o.io.err = handle_device_write(&message.i.io.oid,
+					message.i.data, message.i.size, message.i.io.offs);
+				break;
+
+			default:
+				message.o.io.err = -ENOSYS;
+				break;
+		}
+
+		/* Dispatch final response back to client */
+		msgRespond(server_oid->port, &message, request_id);
+	}
+}
+
+
+int main(void)
+{
+	oid_t server_oid;
+
+	/* Setup system structure */
+	server_oid.id = 0;
+	storage_len = 0;
+	memset(storage_buf, 0, BUF_SIZE);
+
+	/* Initialize IPC communications port */
+	if (portCreate(&server_oid.port) < 0) {
+		fprintf(stderr, "caesar_decode: Failed to create IPC port\n");
+		return EXIT_FAILURE;
+	}
+
+	/* Map the handler to device nodes under /dev */
+	if (create_dev(&server_oid, "caesar_decode") < 0) {
+		fprintf(stderr, "caesar_decode: Failed to map device in filesystem\n");
+		return EXIT_FAILURE;
+	}
+
+	printf("caesar_decode: Server is up at /dev/caesar_decode\n");
+
+	/* Enter receiving loop */
+	run_server_message_loop(&server_oid);
+
+	return EXIT_FAILURE;
+}
